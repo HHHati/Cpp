de382ce9c7a8bb0f2031ede88b7ea7ae6cdea195
Basile De Leeuw <bade-lee@mini-de-basile.home>
2023-01-11 14:58:14 +0100

    cpp05 2.0

--- cpp05/ex01/main.cpp
+++ cpp05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bade-lee <bade-lee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 21:06:11 by bade-lee          #+#    #+#             */
-/*   Updated: 2023/01/09 01:05:39 by bade-lee         ###   ########.fr       */
+/*   Updated: 2023/01/11 14:39:13 by bade-lee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,55 +15,69 @@
 
 int main()
 {
-	try
-	{
-		std::cout << "\n============ creating bureaucrats ============\n" << std::endl;
+	std::cout << "\n============ creating bureaucrats ============\n" << std::endl;
 
-		Bureaucrat boss("boss", 1);
-		std::cout << boss << std::endl;
+	Bureaucrat boss("boss", 1);
+	std::cout << boss << std::endl;
 
-		Bureaucrat employee("employee", 75);
-		std::cout << employee << std::endl;
+	Bureaucrat employee("employee", 75);
+	std::cout << employee << std::endl;
 
-		Bureaucrat intern("intern", 150);
-		std::cout << intern << std::endl;
+	Bureaucrat intern("intern", 150);
+	std::cout << intern << std::endl;
 
-		std::cout << "\n============ upgrade bureaucrats =============\n" << std::endl;
+	std::cout << "\n============ upgrade bureaucrats =============\n" << std::endl;
 
-		employee.upGrade();
-		std::cout << employee << std::endl;
-		intern.upGrade();
-		std::cout << intern << std::endl;
+	employee.upGrade();
+	std::cout << employee << std::endl;
+	intern.upGrade();
+	std::cout << intern << std::endl;
 
-		std::cout << "\n=========== downgrade bureaucrats ============\n" << std::endl;
+	std::cout << "\n=========== downgrade bureaucrats ============\n" << std::endl;
 
-		employee.downGrade();
-		std::cout << employee << std::endl;
-		intern.downGrade();
-		std::cout << intern << std::endl;
+	employee.downGrade();
+	std::cout << employee << std::endl;
+	intern.downGrade();
+	std::cout << intern << std::endl;
 
-		std::cout << "\n=============== creating forms ===============\n" << std::endl;
+	std::cout << "\n=============== creating forms ===============\n" << std::endl;
 
-		Form form1("form1", 150, 150);
-		std::cout << form1 << std::endl;
-		Form form2("form2", 1, 1);
-		std::cout << form2 << std::endl;
+	Form form1("form1", 150, 150);
+	std::cout << form1 << std::endl;
+	Form form2("form2", 1, 1);
+	std::cout << form2 << std::endl;
 
-		std::cout << "\n=============== signing forms ================\n" << std::endl;
+	std::cout << "\n=============== signing forms ================\n" << std::endl;
 
-		form1.beSigned(boss);
-		std::cout << form1 << std::endl;
-		boss.signForm(form1);
+	form1.beSigned(boss);
+	std::cout << form1 << std::endl;
+	boss.signForm(form1);
 
-		std::cout << "\n============= testing exceptions =============\n" << std::endl;
+	std::cout << "\n============= testing exceptions =============\n" << std::endl;
+	
+	try { Bureaucrat toohigh("toohigh", 0); }
+	catch(const std::exception& e) { std::cerr << e.what() << std::endl; }
 
-		// employee.signForm(form2);
-		// form2.beSigned(employee);
-		// Form form3("form3", 160, 160);
-		// Form form4("form4", 0, 0);
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
+	try { Bureaucrat toolow("toolow", 160); }
+	catch(const std::exception& e) { std::cerr << e.what() << std::endl; }
+
+	try { boss.upGrade(); }
+	catch(const std::exception& e) { std::cerr << e.what() << std::endl; }
+		
+	try { intern.downGrade(); }
+	catch(const std::exception& e) { std::cerr << e.what() << std::endl; }
+
+	try { employee.signForm(form2); }
+	catch(const std::exception& e) { std::cerr << e.what() << std::endl; }
+
+	try { form2.beSigned(employee); }
+	catch(const std::exception& e) { std::cerr << e.what() << std::endl; }
+
+	try { Form form3("form3", 160, 160); }
+	catch(const std::exception& e) { std::cerr << e.what() << std::endl; }
+
+	try { Form form4("form4", 0, 0); }
+	catch(const std::exception& e) { std::cerr << e.what() << std::endl; }
+	
+	std::cout << "\n==============================================\n" << std::endl;
 }
