31682e2aa50a2b332f7b8438dfc2aeba45f8b2dc
Basile De leeuw <bade-lee@fu-r10-p5.s19.be>
2023-01-24 12:04:40 +0100

    cpp01 4.0

--- cpp01/ex01/Zombie.cpp
+++ cpp01/ex01/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: bade-lee <bade-lee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 17:31:19 by bade-lee          #+#    #+#             */
-/*   Updated: 2023/01/06 19:06:42 by bade-lee         ###   ########.fr       */
+/*   Updated: 2023/01/24 11:50:08 by bade-lee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 Zombie::Zombie(void)
 {
+	std::cout << "Zombie has been created !" << std::endl;
 }
 
 Zombie::Zombie(std::string name)
