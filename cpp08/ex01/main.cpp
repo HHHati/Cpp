0bf8e6b9c4ee9d502f13985b740b358731c29370
Basile De leeuw <bade-lee@fu-r10-p4.s19.be>
2023-02-16 14:14:05 +0100

    cpp08 1.0

---
+++ cpp08/ex01/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bade-lee <bade-lee@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/16 14:09:29 by bade-lee          #+#    #+#             */
+/*   Updated: 2023/02/16 14:10:27 by bade-lee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "span.hpp"
+
+int main()
+{
+	Span sp = Span(5);
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
+	return 0;
+}
