211b09d55f2dd0a440a224aa6dbd9361cfcf972e
Basile De leeuw <bade-lee@fu-c2-p5.s19.be>
2023-02-28 12:33:01 +0100

    cpp04 5.0

--- cpp04/ex02/AAnimal.cpp
+++ cpp04/ex02/AAnimal.cpp
@@ -6,7 +6,7 @@
 /*   By: bade-lee <bade-lee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 13:24:15 by bade-lee          #+#    #+#             */
-/*   Updated: 2023/01/08 17:11:44 by bade-lee         ###   ########.fr       */
+/*   Updated: 2023/02/28 12:32:24 by bade-lee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,11 +32,6 @@ AAnimal& AAnimal::operator=(const AAnimal& from)
 {
 	this->_type = from._type;
 	return *this;
-}
-
-void AAnimal::makeSound() const
-{
-	std::cout << "The animal doesn't make any specific sound" << std::endl;
 }
 
 const std::string AAnimal::getType() const
