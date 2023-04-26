0d8ad4d02076d28f1fcf54be25e2c31df562fde6
Basile De Leeuw <bade-lee@mini-de-basile.home>
2023-04-26 11:24:46 +0200

    cpp06 3.0

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bade-lee <bade-lee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 14:34:49 by bade-lee          #+#    #+#             */
-/*   Updated: 2023/04/17 14:46:14 by bade-lee         ###   ########.fr       */
+/*   Updated: 2023/04/26 11:18:19 by bade-lee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	main()
 {
 	Data* ptr = new Data;
-	std::cout << "Base poiner  : " << ptr << std::endl;
+	std::cout << "Base pointer  : " << ptr << std::endl;
 
 	uintptr_t serialized = serialize(ptr);
 	std::cout << "Serialized   : " << serialized << std::endl;
