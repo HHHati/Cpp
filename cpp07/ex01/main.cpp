eba84e62ac000ff2dfc73831825fbb34487658c0
Basile De leeuw <bade-lee@fu-r7-p4.s19.be>
2023-05-04 13:44:13 +0200

    cpp07 5.5

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -6,27 +6,18 @@
 /*   By: bade-lee <bade-lee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 14:00:56 by bade-lee          #+#    #+#             */
-/*   Updated: 2023/05/04 13:32:10 by bade-lee         ###   ########.fr       */
+/*   Updated: 2023/05/04 13:43:31 by bade-lee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
-class Awesome
+int	main()
 {
-public:
-Awesome( void ) : _n( 42 ) { return; }
-int get( void ) const { return this->_n; }
-private:
-int _n;
-};
-std::ostream & operator<<( std::ostream & o, Awesome const & rhs ) { o << rhs.get(); return o; }
-template< typename T >
-void print( T const & x ) { std::cout << x << std::endl; return; }
-int main() {
-int tab[] = { 0, 1, 2, 3, 4 }; // <--- I never understood why you can't write int[] tab. Wouldn't that make more sense?
-Awesome tab2[5];
-iter( tab, 5, print );
-iter( tab2, 5, print );
-return 0;
+	std::cout << "Test with char :\n" << std::endl;
+	std::string test = "This is a test.";
+	iter<const char>(test.c_str(), test.length(), print);
+	std::cout << "\n\nTest with int :\n" << std::endl;
+	int tab[] = {1, 2, 3, 4, 5};
+	iter<int>(tab, 5, print);
 }
