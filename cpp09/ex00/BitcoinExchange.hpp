719d01f7efff0716510489fdcd022538d2a4232b
Basile De Leeuw <basile_deleeuw@yahoo.be>
2023-05-23 16:26:32 +0200

    cpp09 1.1

--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,31 @@
 /*   By: bade-lee <bade-lee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 14:39:31 by bade-lee          #+#    #+#             */
-/*   Updated: 2023/05/23 14:39:32 by bade-lee         ###   ########.fr       */
+/*   Updated: 2023/05/23 16:26:14 by bade-lee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <string>
+#include <map>
+
+class BitcoinExchange{
+
+	public:
+
+		BitcoinExchange();
+		BitcoinExchange(const BitcoinExchange& from);
+		~BitcoinExchange();
+
+		BitcoinExchange& operator=(const BitcoinExchange& from);
+
+		
+	
+	private:
+	
+		oui
+};
+
+#endif
