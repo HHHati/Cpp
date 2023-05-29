3b40539258960fd077e3887b406047b0aba00bf9
Basile De Leeuw <basile_deleeuw@yahoo.be>
2023-05-29 16:07:07 +0200

    cpp09 2.0

--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -6,31 +6,40 @@
 /*   By: bade-lee <bade-lee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 14:39:31 by bade-lee          #+#    #+#             */
-/*   Updated: 2023/05/23 16:26:14 by bade-lee         ###   ########.fr       */
+/*   Updated: 2023/05/29 15:53:37 by bade-lee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BITCOINEXCHANGE_HPP
-#define BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
 
-#include <string>
-#include <map>
+# include <iostream>
+# include <fstream>
+# include <sstream>
+# include <map>
+# include <string>
+# include <algorithm>
 
 class BitcoinExchange{
 
 	public:
 
 		BitcoinExchange();
+		BitcoinExchange(const std::string &filename);
 		BitcoinExchange(const BitcoinExchange& from);
 		~BitcoinExchange();
 
 		BitcoinExchange& operator=(const BitcoinExchange& from);
 
-		
-	
+		void 	readData(const std::string &filename);
+		double	getExchangeRate(const std::string &date);
+		bool	isValidDate(const std::string& date);
+		bool	isValidValue(const double &value);
+
+
 	private:
 	
-		oui
+		std::map<std::string, double> _data;
 };
 
 #endif
--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -6,8 +6,20 @@
 /*   By: bade-lee <bade-lee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 14:39:33 by bade-lee          #+#    #+#             */
-/*   Updated: 2023/05/23 16:06:13 by bade-lee         ###   ########.fr       */
+/*   Updated: 2023/05/29 15:41:06 by bade-lee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
+
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cerr << "Error: Invalid number of arguments." << "\n";
+		return 1;
+	}
+	BitcoinExchange exchange("data.csv");
+	exchange.readData(argv[1]);
+	return 0;
+}
